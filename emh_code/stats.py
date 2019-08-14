d6c863ec68279aabe305e7e5b85b31305b3e8346
Eric Holloway <eric.holloway@gmail.com>
2019-08-14 09:20:15 -0400

    stats.py for descriptive stats.

--- emh_code/stats.py
+++ emh_code/stats.py
@@ -1,6 +1,11 @@
 import csv
+import sys
 import pandas as pd
 
-meta = pd.read_csv('../target_srr_metadata.csv')
+filename = '../target_srr_metadata.csv'
+if len(sys.argv) > 1:
+    filename = sys.argv[1]
 
-meta.describe()
+meta = pd.read_csv(filename)
+
+print(meta.describe())
